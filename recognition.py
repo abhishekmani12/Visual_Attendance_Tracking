bbe91f062515a3d103482b1f3732fb9951705225
Abhishek Manikandan <abhishekmanikan12@gmail.com>
2023-02-16 19:30:41 +0530

    Added KNN

--- recognition.py
+++ recognition.py
@@ -7,7 +7,7 @@ import os
 import pickle as pkl
 
 
-def train(algorithm="linear"): #classifier train function
+def train(algorithm="svm"): #classifier train function
     encodings = []
     face_id = []
 
@@ -18,30 +18,36 @@ def train(algorithm="linear"): #classifier train function
     for face in tqdm(master): #loop through subfolders in Dataset folder
         sub=os.listdir("Dataset/" +face)
         print(f"{face}'s face encoding in progress")
-        for faceimg in sub: #loop throuigh images present in each sub folder
+        for faceimg in sub: #loop through images present in each sub folder
             file="Dataset/" + face + "/" + faceimg
             vals=emb.encoding(file,"hog") #pass file to encodings function of embedding script to get 128 face encoding values for each face
             encodings.append(vals) #appending encodings to list
             face_id.append(face) #append face name to list
     
-    if algorithm == "linear": #setting the required kernel for svm
-        kern='linear'
-    elif algorithm == "rbf":
-        kern="rbf"
+    if algorithm == "svm": #setting the required kernel for svm
         
-    clf1 = svm.SVC(C=7766.325241554844, kernel=kern, gamma='auto') #instantiate svm
-    clf1.fit(encodings,face_id) #train
-    
+        clf1 = svm.SVC(C=7766.325241554844, kernel="rbf", gamma='auto') #instantiate svm
+        clf1.fit(encodings,face_id) #train
+        
+        pkl.dump(clf1,open('models/svm_classifier.pkl','wb')) #save
+        print("SVM Model pickled")
+        return clf1 #return model object
+        
+    elif algorithm == "knn":
+        
+        n=int(round(math.sqrt(len(encodings))))
+        knn=neighbors.KNeighborsClassifier(n_neighbors=n, weights='distance')
+        knn.fit(encodings, face_id)
+        
+        pkl.dump(knn,open('models/knn_classifier.pkl','wb')) #save
+        print("KNN Model pickled")
+        return knn #return model object
+        
     '''
     res=sklearn.model_selection.cross_val_score(clf1, encodings, face_id, n_jobs=-1, cv=3)
     accuracy=res.mean()
     print(accuracy)
     '''
-    
-    pkl.dump(clf1,open('models/classifier.pkl','wb')) #save
-    print("SVM Model pickled")
-    return clf1 #return model object
-
         
 def compressor(file): #image compressor function
     
@@ -68,6 +74,11 @@ def pred(file, model): #prediction function - pass image file and required model
     
     return res[0] #return 0th index of the list of list result
 
-def loadmodel(): #Load trained model from pkl format
-    model=pkl.load(open('models/classifier.pkl', 'rb'))
+def load_model(algorithm="svm"): #Load trained model from pkl format
+    
+    if algorithm=="svm":
+        model=pkl.load(open('models/classifier.pkl', 'rb')) #svm
+        
+    elif algorithm=="knn":
+        model=pkl.load(open('models/knn_classifier.pkl', 'rb')) #knn
     return model
